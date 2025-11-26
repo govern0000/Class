0c6d01f343f5586859c48cc496e9e3efcac5e212
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-26 12:12:25 +0800

    Saber

--- Saber/Saber.Node/SetStateArg.cs
+++ Saber/Saber.Node/SetStateArg.cs
@@ -2,8 +2,8 @@ namespace Saber.Node;
 
 public class SetStateArg : Any
 {
-    public virtual Node Node { get; set; }
     public virtual NodeKind Kind { get; set; }
+    public virtual Node Node { get; set; }
     public virtual object Field00 { get; set; }
     public virtual object Field01 { get; set; }
     public virtual object Field02 { get; set; }
