1cca09987d32fb3a70080535b27c2b5260628f64
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-26 12:34:50 +0800

    Saber

--- Saber/Saber.Node/NodeKind.cs
+++ Saber/Saber.Node/NodeKind.cs
@@ -5,7 +5,7 @@ public class NodeKind : Any
     public virtual long Index { get; set; }
     public virtual String Name { get; set; }
     public virtual Node Node { get; set; }
-    public virtual InfraState NewState { get; set; }
     public virtual NodeState NodeState { get; set; }
+    public virtual NewState NewState { get; set; }
     public virtual SetState SetState { get; set; }
 }
