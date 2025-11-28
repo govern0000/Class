b341a3085393cf719245443820d36c49427a2c62
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-29 02:24:20 +0800

    Infra

--- Infra/Infra/Environ.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdlib.h>
-#include <string.h>
-
-#include "Pronate.h"
-
-Int Environ_TrigAlloc(Int k, Int memory);
---
+++ Infra/Infra/Environ.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdlib>
+#include <cstring>
+
+#include "Pronate.hpp"
+
+Int Environ_TrigAlloc(Int k, Int memory);
