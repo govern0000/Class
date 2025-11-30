15c51bdecef8eb13545b74cf8bb3027ff0958c38
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-11-30 11:37:01 +0800

    Script

--- Script/Saber/CleanModule.cmd
+++ Script/Saber/CleanModule.cmd
@@ -4,5 +4,5 @@ echo Clean Module
 set DotNetModuleOutFold=.\Out\net8.0
 del /F /Q %DotNetModuleOutFold%\Saber.* 2>NUL
 del /F /Q %DotNetModuleOutFold%\SaberExe* 2>NUL
-rmdir /S /Q %DotNetModuleOutFold%\Saber.Console.data 2>NUL
+rmdir /S /Q %DotNetModuleOutFold%\Saber.Class.data 2>NUL
 rmdir /S /Q %DotNetModuleOutFold%\Saber.Doc.data 2>NUL
