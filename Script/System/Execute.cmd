951e5cc3a7a5f93eb9e69dfd77e91fd5a5ed8d73
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-12-01 10:21:40 +0800

    Script

--- Script/System/Execute.cmd
+++ Script/System/Execute.cmd
@@ -1,6 +1,9 @@
 @echo off
 
-pushd Out\Class
-SystemDemo-96207.08.47.exe
+set ClassFold=C:\Users\aaabb\Package\Class\Out\Class
+
+pushd Out\Class\Module
+
+set "PATH=%ClassFold%;%PATH%" & set "QT_PLUGIN_PATH=%ClassFold%" & ..\Library\SystemDemo-96207.08.47.exe
 echo Status: %errorlevel%
 popd
