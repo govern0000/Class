477635cff9c8af77a4d17d42fb6de3328193e799
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-12-02 19:17:09 +0800

    Saber

--- Saber/Saber.Class/LibraryGen.cs
+++ Saber/Saber.Class/LibraryGen.cs
@@ -717,6 +717,7 @@ public class LibraryGen : TextAdd
             list.Add(this.S("-Wl,-subsystem,console"));
         }
 
+        list.Add(this.AddClear().AddS("-L../../../").Add(this.ClassPath).Add(this.TextInfra.PathCombine).AddS("Infra").AddResult());
         list.Add(this.AddClear().AddS("-L../../../").Add(this.LibraryPath).AddResult());
         list.Add(this.S("-lInfra"));
         list.Add(this.S("-lInfraIntern"));
