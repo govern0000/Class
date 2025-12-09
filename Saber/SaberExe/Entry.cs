be19c674e5c907f9eca9268c131f92e2c8bf2f95
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-12-09 23:23:28 +0800

    Saber

--- Saber/SaberExe/Entry.cs
+++ Saber/SaberExe/Entry.cs
@@ -5,12 +5,6 @@ class Entry
     [STAThread]
     static int Main(string[] arg)
     {
-        EntryEntry entry;
-        entry = new ModuleEntry();
-        entry.Init();
-        entry.ArgSet(arg);
-        int o;
-        o = entry.Execute();
-        return o;
+        return EntryEntry.Main(new ModuleEntry(), arg);
     }
 }
