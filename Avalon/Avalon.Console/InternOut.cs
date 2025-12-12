05cb1aed13aac1d691cf65c2b939941cdb685ad8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-12-13 04:52:11 +0800

    Avalon

--- Avalon/Avalon.Console/InternOut.cs
+++ Avalon/Avalon.Console/InternOut.cs
@@ -2,5 +2,13 @@ namespace Avalon.Console;
 
 class InternOut : StringOut
 {
-    
+    public override bool Write(String value)
+    {
+        Intern.This.Phore.Open();
+
+        base.Write(value);
+
+        Intern.This.Phore.Close();
+        return true;
+    }
 }
