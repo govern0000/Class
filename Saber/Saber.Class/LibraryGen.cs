a2e47d08b92dac91043f686b741f31d8258be63b
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-05 15:37:19 +0800

    Saber

--- Saber/Saber.Class/LibraryGen.cs
+++ Saber/Saber.Class/LibraryGen.cs
@@ -199,6 +199,7 @@ public partial class LibraryGen : TextAdd
     public virtual BinaryBinary Binary { get; set; }
     public virtual long ModuleCount { get; set; }
     public virtual ClassModule SystemInfraModule { get; set; }
+    public virtual ClassClass EntryClass { get; set; }
     public virtual SystemClass System { get; set; }
     public virtual Array InitArray { get; set; }
     public virtual Array BaseArray { get; set; }
