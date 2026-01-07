18d56a17924cf82e17e76ea26409eb2cb0b4d610
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-07 15:09:59 +0800

    Infra

--- Infra/Infra/FrameIntern.cpp
+++ Infra/Infra/FrameIntern.cpp
@@ -26,23 +26,6 @@ void FrameIntern::paintEvent(QPaintEvent* ev)
     this->DrawEventHandle();
 }
 
-void FrameIntern::changeEvent(QEvent* ev)
-{
-    if (ev->type() == QEvent::WindowStateChange)
-    {
-        QWindowStateChangeEvent* ka;
-        ka = static_cast<QWindowStateChangeEvent*>(ev);
-
-        unsigned int kd;
-        kd = ka->oldState().toInt();
-
-        Int k;
-        k = kd;
-
-        this->CaseEventHandle(k);
-    }
-}
-
 Int FrameIntern::TypeEventHandle(Int index, Int value)
 {
     Int frame;
@@ -58,23 +41,5 @@ Int FrameIntern::DrawEventHandle()
     frame = this->Frame;
 
     Frame_DrawEvent(frame);
-    return true;
-}
-
-Int FrameIntern::SizeEventHandle()
-{
-    Int frame;
-    frame = this->Frame;
-
-    Frame_SizeEvent(frame);
-    return true;
-}
-
-Int FrameIntern::CaseEventHandle(Int prev)
-{
-    Int frame;
-    frame = this->Frame;
-
-    Frame_CaseEvent(frame, prev);
     return true;
 }
