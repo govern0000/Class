9f0bac23aaa6ddf3fe49c4e371185395186d1af1
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-07 16:32:54 +0800

    Infra

--- Infra/Infra/FrameIntern.hpp
+++ Infra/Infra/FrameIntern.hpp
@@ -29,5 +29,5 @@ protected:
 private:
     Int TypeEventHandle(Int index, Int value);
     Int DrawEventHandle();
-    Int PointerEventHandle(Int kind, Int index, Int valueA, Int valueB, Int valueC, Int valueD);
+    Int PointEventHandle(Int kind, Int index, Int valueA, Int valueB, Int valueC, Int valueD);
 };
