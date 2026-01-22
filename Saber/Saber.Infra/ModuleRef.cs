5390d26477869298842670cbb89ae99576314500
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-22 17:10:27 +0800

    Saber

--- Saber/Saber.Infra/ModuleRef.cs
+++ Saber/Saber.Infra/ModuleRef.cs
@@ -2,6 +2,7 @@ namespace Saber.Infra;
 
 public class ModuleRef : Any
 {
+    public virtual String Acount { get; set; }
     public virtual String Name { get; set; }
     public virtual long Ver { get; set; }
 }
