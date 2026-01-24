1dc0be3e59e6744685aeeb86106aee2bbeacc960
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-24 17:03:15 +0800

    Script

--- Script/System/LibraryDemo.cmd
+++ Script/System/LibraryDemo.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-call ..\Script\Module\Library SystemDemo-96207.08.47
-call ..\Script\Module\Library SystemDemoNetwork-0.00.00
+call ..\Script\Module\Library Alan-SystemDemo-96207.08.47
+call ..\Script\Module\Library Alan-SystemDemoNetwork-0.00.00
