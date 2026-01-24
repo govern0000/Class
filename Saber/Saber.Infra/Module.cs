cebb7aa43439802370968ddc568989348785040c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-24 14:55:02 +0800

    Saber

--- Saber/Saber.Infra/Module.cs
+++ Saber/Saber.Infra/Module.cs
@@ -3,16 +3,10 @@ namespace Saber.Infra;
 public class Module : Any
 {
     public virtual ModuleRef Ref { get; set; }
-
     public virtual Table Class { get; set; }
-
     public virtual Table Import { get; set; }
-
     public virtual Table Export { get; set; }
-
     public virtual Table Storage { get; set; }
-
     public virtual String Entry { get; set; }
-
     public virtual object Any { get; set; }
 }
