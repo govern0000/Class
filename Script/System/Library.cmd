9b1a9626bef70f165460e4cbaa71ec43867c7f78
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-24 17:02:34 +0800

    Script

--- Script/System/Library.cmd
+++ Script/System/Library.cmd
@@ -1,14 +1,14 @@
 @echo off
 
-call ..\Script\Module\Library System.Infra-0.00.00
-call ..\Script\Module\Library System.List-0.00.00
-call ..\Script\Module\Library System.Math-0.00.00
-call ..\Script\Module\Library System.Text-0.00.00
-call ..\Script\Module\Library System.Thread-0.00.00
-call ..\Script\Module\Library System.Stream-0.00.00
-call ..\Script\Module\Library System.Time-0.00.00
-call ..\Script\Module\Library System.Storage-0.00.00
-call ..\Script\Module\Library System.Network-0.00.00
-call ..\Script\Module\Library System.Console-0.00.00
-call ..\Script\Module\Library System.Entry-0.00.00
+call ..\Script\Module\Library System-System.Infra-0.00.00
+call ..\Script\Module\Library System-System.List-0.00.00
+call ..\Script\Module\Library System-System.Math-0.00.00
+call ..\Script\Module\Library System-System.Text-0.00.00
+call ..\Script\Module\Library System-System.Thread-0.00.00
+call ..\Script\Module\Library System-System.Stream-0.00.00
+call ..\Script\Module\Library System-System.Time-0.00.00
+call ..\Script\Module\Library System-System.Storage-0.00.00
+call ..\Script\Module\Library System-System.Network-0.00.00
+call ..\Script\Module\Library System-System.Console-0.00.00
+call ..\Script\Module\Library System-System.Entry-0.00.00
 call Script\System\LibraryDemo
