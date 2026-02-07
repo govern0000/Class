3d74627aa58c6fb51ac87127e2a39aa608700f8d
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-07 14:58:16 +0800

    Avalon

--- Avalon/Avalon.Infra/ExternConstant.cs
+++ Avalon/Avalon.Infra/ExternConstant.cs
@@ -2,7 +2,7 @@ namespace Avalon.Infra;
 
 static class ExternConstant
 {
-    public const string NameBefore = "";
+    public const string NameBefore = "Infra/";
 
     public const string NameAfter = "";
 }
