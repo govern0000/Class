ebb1f3ce0df55275ae1bcb8c347b202d8b4641a4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-07 17:03:09 +0800

    Script

--- Script/System/Execute.cmd
+++ Script/System/Execute.cmd
@@ -1,8 +1,6 @@
 @echo off
 
-set InfraFold=C:\Users\aaabb\Package\Class\Out\Class\Infra
-
 pushd Out\Class\Module
-set "QT_PLUGIN_PATH=%InfraFold%" & ..\Library\Alan-SystemDemo-96207.08.47.exe "-"
+..\Library\Alan-SystemDemo-96207.08.47.exe "-"
 echo Status: %errorlevel%
 popd
