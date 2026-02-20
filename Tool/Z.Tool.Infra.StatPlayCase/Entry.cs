4787574efd7912723f95f31178b0ddb661fe68ce
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-20 11:34:46 +0800

    Tool

---
+++ Tool/Z.Tool.Infra.StatPlayCase/Entry.cs
@@ -0,0 +1,20 @@
+namespace Z.Tool.Infra.StatPlayCase;
+
+class Entry : EntryEntry
+{
+    protected override long ExecuteMain()
+    {
+        Gen gen;
+        gen = new Gen();
+        gen.Init();
+        long o;
+        o = gen.Execute();
+        return o;
+    }
+
+    [STAThread]
+    static int Main(string[] arg)
+    {
+        return EntryEntry.Main(new Entry(), arg);
+    }
+}
---
+++ Tool/Z.Tool.Infra.StatPlayCase/Gen.cs
@@ -0,0 +1,15 @@
+namespace Z.Tool.Infra.StatPlayCase;
+
+class Gen : StatGen
+{
+    public override long Execute()
+    {
+        this.ClassName = this.S("NetworkCase");
+        this.ScopeName = this.S("QAbstractSocket");
+        this.ValuePostfix = this.S("State");
+        this.ValueOffset = this.S(" + 1");
+        this.ItemListFileName = this.S("ToolData/Infra/ItemListNetworkCase.txt");
+
+        return base.Execute();
+    }
+}
