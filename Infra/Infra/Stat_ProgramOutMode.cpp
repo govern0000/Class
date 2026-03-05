ea903df186d9d95fb0a4e6a5ffa66c40b53bc146
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-05 22:16:16 +0800

    Infra

--- Infra/Infra/Stat_ProgramOutMode.cpp
+++ Infra/Infra/Stat_ProgramOutMode.cpp
@@ -3,8 +3,8 @@
 Int Stat_Var_ProgramOutModeSeparateChannels = QProcess::SeparateChannels + 1;
 Int Stat_Var_ProgramOutModeMergedChannels = QProcess::MergedChannels + 1;
 Int Stat_Var_ProgramOutModeForwardedChannels = QProcess::ForwardedChannels + 1;
-Int Stat_Var_ProgramOutModeForwardedErrorChannel = QProcess::ForwardedErrorChannel + 1;
 Int Stat_Var_ProgramOutModeForwardedOutputChannel = QProcess::ForwardedOutputChannel + 1;
+Int Stat_Var_ProgramOutModeForwardedErrorChannel = QProcess::ForwardedErrorChannel + 1;
 
 Int Stat_ProgramOutModeSeparateChannels(Int o)
 {
@@ -18,11 +18,11 @@ Int Stat_ProgramOutModeForwardedChannels(Int o)
 {
     return Stat_Var_ProgramOutModeForwardedChannels;
 }
-Int Stat_ProgramOutModeForwardedErrorChannel(Int o)
-{
-    return Stat_Var_ProgramOutModeForwardedErrorChannel;
-}
 Int Stat_ProgramOutModeForwardedOutputChannel(Int o)
 {
     return Stat_Var_ProgramOutModeForwardedOutputChannel;
+}
+Int Stat_ProgramOutModeForwardedErrorChannel(Int o)
+{
+    return Stat_Var_ProgramOutModeForwardedErrorChannel;
 }
