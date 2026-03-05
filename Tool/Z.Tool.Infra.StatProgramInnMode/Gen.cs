429d79c9c713013f1814fac863a36f52c40dfa47
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-05 21:55:14 +0800

    Tool

--- Tool/Z.Tool.Infra.StatProgramInnMode/Gen.cs
+++ Tool/Z.Tool.Infra.StatProgramInnMode/Gen.cs
@@ -4,11 +4,11 @@ class Gen : StatGen
 {
     public override long Execute()
     {
-        this.ClassName = this.S("PlayCase");
-        this.ScopeName = this.S("QMediaPlayer");
-        this.ValuePostfix = this.S("State");
+        this.ClassName = this.S("ProgramInnMode");
+        this.ScopeName = this.S("QProcess");
+        this.ValuePostfix = this.S("InputChannel");
         this.ValueOffset = this.S(" + 1");
-        this.ItemListFileName = this.S("ToolData/Infra/ItemListPlayCase.txt");
+        this.ItemListFileName = this.S("ToolData/Infra/ItemListProgramInnMode.txt");
 
         return base.Execute();
     }
