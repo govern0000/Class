a3b052e244a6f9d89ddd192e16f19bae32ede005
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-06 14:53:45 +0800

    Tool

---
+++ Tool/Z.Tool.Avalon.ProgramInnModeList/-.cs
@@ -0,0 +1,4 @@
+global using String = Avalon.Infra.String;
+global using EntryEntry = Avalon.Entry.Entry;
+global using SourceGen = Z.Infra.StatItemListSourceGen.AvalonGen;
+global using STAThreadAttribute = System.STAThreadAttribute;
---
+++ Tool/Z.Tool.Avalon.ProgramInnModeList/Gen.cs
@@ -0,0 +1,19 @@
+namespace Z.Tool.Avalon.ProgramOutModeList;
+
+public class Gen : SourceGen
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.Module = this.S("Avalon.Console");
+        this.ClassName = this.S("ProgramOutModeList");
+        this.BaseClassName = this.S("Any");
+        this.AnyClassName = this.S("Any");
+        this.ItemClassName = this.S("ProgramOutMode");
+        this.ArrayClassName = this.S("Array");
+        this.Export = true;
+        this.StatItemClassName = this.S("ProgramOutMode");
+        this.ItemListFileName = this.GetStatItemListFileName();
+        return true;
+    }
+}
