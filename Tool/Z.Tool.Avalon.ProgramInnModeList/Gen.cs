a7471c49177ab18a2efdf6477d556e88117ac7b4
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-06 14:55:05 +0800

    Tool

--- Tool/Z.Tool.Avalon.ProgramInnModeList/Gen.cs
+++ Tool/Z.Tool.Avalon.ProgramInnModeList/Gen.cs
@@ -6,13 +6,13 @@ public class Gen : SourceGen
     {
         base.Init();
         this.Module = this.S("Avalon.Console");
-        this.ClassName = this.S("ProgramOutModeList");
+        this.ClassName = this.S("ProgramInnModeList");
         this.BaseClassName = this.S("Any");
         this.AnyClassName = this.S("Any");
-        this.ItemClassName = this.S("ProgramOutMode");
+        this.ItemClassName = this.S("ProgramInnMode");
         this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.StatItemClassName = this.S("ProgramOutMode");
+        this.StatItemClassName = this.S("ProgramInnMode");
         this.ItemListFileName = this.GetStatItemListFileName();
         return true;
     }
