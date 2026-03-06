3a61bef0f21c883f969e33a009d34d807e3cdfc5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-06 14:29:21 +0800

    Avalon

---
+++ Avalon/Avalon.Infra/IntLess.cs
@@ -0,0 +1,27 @@
+namespace Avalon.Infra;
+
+class IntLess : Less
+{
+    public override bool Init()
+    {
+        base.Init();
+        this.LessInt = new LessInt();
+        this.LessInt.Init();
+        return true;
+    }
+
+    protected virtual LessInt LessInt { get; set; }
+
+    public override long Execute(object lite, object rite)
+    {
+        Value liteValue;
+        Value riteValue;
+        liteValue = lite as Value;
+        riteValue = rite as Value;
+
+        long k;
+        k = this.LessInt.Execute(liteValue.Int, riteValue.Int);
+
+        return k;
+    }
+}
