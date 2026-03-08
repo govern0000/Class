3a425aa3674936700c6a83142166660a4e46fc2a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-08 10:31:14 +0800

    Script

--- Script/System/Execute.cmd
+++ Script/System/Execute.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set InfraLibraryFold=C:\Users\aaabb\Package\Class\Out\Infra
+set InfraLibraryFold=C:\Users\aaabb\Package\Class\Out\Class\Infra
 
 pushd Out\Class\Module
 set "PATH=%InfraLibraryFold%;%PATH%" && ..\Library\Alan-SystemDemo-96207.08.47.exe "-" "-"
