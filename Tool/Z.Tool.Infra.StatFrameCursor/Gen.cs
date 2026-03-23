b8f551ec32632015f18bd35fdfc2b4217b06aed9
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-23 13:46:44 +0800

    Tool

--- Tool/Z.Tool.Infra.StatFrameCursor/Gen.cs
+++ Tool/Z.Tool.Infra.StatFrameCursor/Gen.cs
@@ -4,11 +4,10 @@ class Gen : StatGen
 {
     public override long Execute()
     {
-        this.ClassName = this.S("PlayCase");
-        this.ScopeName = this.S("QMediaPlayer");
-        this.ValuePostfix = this.S("State");
+        this.ClassName = this.S("FrameCursor");
+        this.ValuePostfix = this.S("Cursor");
         this.ValueOffset = this.S(" + 1");
-        this.ItemListFileName = this.S("ToolData/Infra/ItemListPlayCase.txt");
+        this.ItemListFileName = this.S("ToolData/Infra/ItemListFrameCursor.txt");
 
         return base.Execute();
     }
