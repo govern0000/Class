e7c81a43a025986f0d66c25b03d68756178665fb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-23 14:07:12 +0800

    Infra

---
+++ Infra/Infra/Stat_CursorShape.cpp
@@ -0,0 +1,113 @@
+#include "Stat.hpp"
+
+Int Stat_Var_CursorShapeArrow = Qt::ArrowCursor + 1;
+Int Stat_Var_CursorShapeUpArrow = Qt::UpArrowCursor + 1;
+Int Stat_Var_CursorShapeCross = Qt::CrossCursor + 1;
+Int Stat_Var_CursorShapeWait = Qt::WaitCursor + 1;
+Int Stat_Var_CursorShapeIBeam = Qt::IBeamCursor + 1;
+Int Stat_Var_CursorShapeSizeVer = Qt::SizeVerCursor + 1;
+Int Stat_Var_CursorShapeSizeHor = Qt::SizeHorCursor + 1;
+Int Stat_Var_CursorShapeSizeBDiag = Qt::SizeBDiagCursor + 1;
+Int Stat_Var_CursorShapeSizeFDiag = Qt::SizeFDiagCursor + 1;
+Int Stat_Var_CursorShapeSizeAll = Qt::SizeAllCursor + 1;
+Int Stat_Var_CursorShapeBlank = Qt::BlankCursor + 1;
+Int Stat_Var_CursorShapeSplitV = Qt::SplitVCursor + 1;
+Int Stat_Var_CursorShapeSplitH = Qt::SplitHCursor + 1;
+Int Stat_Var_CursorShapePointingHand = Qt::PointingHandCursor + 1;
+Int Stat_Var_CursorShapeForbidden = Qt::ForbiddenCursor + 1;
+Int Stat_Var_CursorShapeWhatsThis = Qt::WhatsThisCursor + 1;
+Int Stat_Var_CursorShapeBusy = Qt::BusyCursor + 1;
+Int Stat_Var_CursorShapeOpenHand = Qt::OpenHandCursor + 1;
+Int Stat_Var_CursorShapeClosedHand = Qt::ClosedHandCursor + 1;
+Int Stat_Var_CursorShapeDragCopy = Qt::DragCopyCursor + 1;
+Int Stat_Var_CursorShapeDragMove = Qt::DragMoveCursor + 1;
+Int Stat_Var_CursorShapeDragLink = Qt::DragLinkCursor + 1;
+
+Int Stat_CursorShapeArrow(Int o)
+{
+    return Stat_Var_CursorShapeArrow;
+}
+Int Stat_CursorShapeUpArrow(Int o)
+{
+    return Stat_Var_CursorShapeUpArrow;
+}
+Int Stat_CursorShapeCross(Int o)
+{
+    return Stat_Var_CursorShapeCross;
+}
+Int Stat_CursorShapeWait(Int o)
+{
+    return Stat_Var_CursorShapeWait;
+}
+Int Stat_CursorShapeIBeam(Int o)
+{
+    return Stat_Var_CursorShapeIBeam;
+}
+Int Stat_CursorShapeSizeVer(Int o)
+{
+    return Stat_Var_CursorShapeSizeVer;
+}
+Int Stat_CursorShapeSizeHor(Int o)
+{
+    return Stat_Var_CursorShapeSizeHor;
+}
+Int Stat_CursorShapeSizeBDiag(Int o)
+{
+    return Stat_Var_CursorShapeSizeBDiag;
+}
+Int Stat_CursorShapeSizeFDiag(Int o)
+{
+    return Stat_Var_CursorShapeSizeFDiag;
+}
+Int Stat_CursorShapeSizeAll(Int o)
+{
+    return Stat_Var_CursorShapeSizeAll;
+}
+Int Stat_CursorShapeBlank(Int o)
+{
+    return Stat_Var_CursorShapeBlank;
+}
+Int Stat_CursorShapeSplitV(Int o)
+{
+    return Stat_Var_CursorShapeSplitV;
+}
+Int Stat_CursorShapeSplitH(Int o)
+{
+    return Stat_Var_CursorShapeSplitH;
+}
+Int Stat_CursorShapePointingHand(Int o)
+{
+    return Stat_Var_CursorShapePointingHand;
+}
+Int Stat_CursorShapeForbidden(Int o)
+{
+    return Stat_Var_CursorShapeForbidden;
+}
+Int Stat_CursorShapeWhatsThis(Int o)
+{
+    return Stat_Var_CursorShapeWhatsThis;
+}
+Int Stat_CursorShapeBusy(Int o)
+{
+    return Stat_Var_CursorShapeBusy;
+}
+Int Stat_CursorShapeOpenHand(Int o)
+{
+    return Stat_Var_CursorShapeOpenHand;
+}
+Int Stat_CursorShapeClosedHand(Int o)
+{
+    return Stat_Var_CursorShapeClosedHand;
+}
+Int Stat_CursorShapeDragCopy(Int o)
+{
+    return Stat_Var_CursorShapeDragCopy;
+}
+Int Stat_CursorShapeDragMove(Int o)
+{
+    return Stat_Var_CursorShapeDragMove;
+}
+Int Stat_CursorShapeDragLink(Int o)
+{
+    return Stat_Var_CursorShapeDragLink;
+}
