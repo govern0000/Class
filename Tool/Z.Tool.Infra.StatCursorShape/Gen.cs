6a74cf852ad98a1aee1cb820811b4d7f89811563
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-23 13:49:04 +0800

    Tool

--- Tool/Z.Tool.Infra.StatCursorShape/Gen.cs
+++ Tool/Z.Tool.Infra.StatCursorShape/Gen.cs
@@ -4,10 +4,10 @@ class Gen : StatGen
 {
     public override long Execute()
     {
-        this.ClassName = this.S("FrameCursor");
+        this.ClassName = this.S("CursorShape");
         this.ValuePostfix = this.S("Cursor");
         this.ValueOffset = this.S(" + 1");
-        this.ItemListFileName = this.S("ToolData/Infra/ItemListFrameCursor.txt");
+        this.ItemListFileName = this.S("ToolData/Infra/ItemListCursorShape.txt");
 
         return base.Execute();
     }
