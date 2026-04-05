fd751c2d15cdeb8ea201990875829bcd64fc98f3
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-05 13:07:32 +0800

    Saber

--- Saber/Saber.Infra/Source.cs
+++ Saber/Saber.Infra/Source.cs
@@ -4,5 +4,6 @@ public class Source : Any
 {
     public virtual long Index { get; set; }
     public virtual String Name { get; set; }
+    public virtual String String { get; set; }
     public virtual Array Text { get; set; }
 }
