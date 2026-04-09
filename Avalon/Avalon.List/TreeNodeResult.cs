a4e08f4f99ec5a49b22e446e4717b40748aa47e8
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-09 16:13:40 +0800

    Avalon

--- Avalon/Avalon.List/TreeNodeResult.cs
+++
@@ -1,9 +0,0 @@
-namespace Avalon.List;
-
-class TreeNodeResult : Any
-{
-    public virtual bool HasNode { get; set; }
-    public virtual TreeNode Node { get; set; }
-    public virtual TreeNode ParentNode { get; set; }
-    public virtual bool ParentLite { get; set; }
-}
