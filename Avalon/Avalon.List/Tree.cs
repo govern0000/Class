07b2db2f2732f8ee13434aee581a8f3105b374d5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-11 05:46:53 +0800

    Avalon

--- Avalon/Avalon.List/Tree.cs
+++ Avalon/Avalon.List/Tree.cs
@@ -2,5 +2,14 @@ namespace Avalon.List;
 
 class Tree : Any
 {
+    public override bool Init()
+    {
+        base.Init();
+        this.Nil = new TreeNode();
+        this.Nil.Init();
+        return true;
+    }
+
     public virtual Less Less { get; set; }
+    protected virtual TreeNode Nil { get; set; }
 }
