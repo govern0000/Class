3be980ff3d49112d458496b92d8b038af28fd514
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-11 06:00:13 +0800

    Avalon

--- Avalon/Avalon.List/Tree.cs
+++ Avalon/Avalon.List/Tree.cs
@@ -11,5 +11,26 @@ class Tree : Any
     }
 
     public virtual Less Less { get; set; }
+    protected virtual TreeNode Root { get; set; }
     protected virtual TreeNode Nil { get; set; }
+
+    protected virtual bool Rotate(TreeNode x)
+    {
+        TreeNode y;
+        y = x.Child(true);
+        
+        x.ChildSet(true,  y.Child(false));
+
+        if (y.Child(false) == this.Nil)
+        {
+            y.Child(false).P = x;
+        }
+
+        y.P = x.P;
+
+        if (x.P == this.Nil)
+        {
+            
+        }
+    }
 }
