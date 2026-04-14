7cef635b0466b88ce1f498249159b70413cfae60
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-14 11:30:26 +0800

    Avalon

--- Avalon/Avalon.List/TreeNode.cs
+++ Avalon/Avalon.List/TreeNode.cs
@@ -5,7 +5,7 @@ class TreeNode : Any
     public virtual TreeNode P { get; set; }
     public virtual TreeNode Lite { get; set; }
     public virtual TreeNode Rite { get; set; }
-    public virtual bool Red { get; set; }
+    public virtual bool Color { get; set; }
     public virtual object Index { get; set; }
     public virtual object Value { get; set; }
 
