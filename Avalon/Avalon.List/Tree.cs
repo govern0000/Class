7d065f0976dd260c975ac025c994fc5227074613
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-04-14 12:30:19 +0800

    Avalon

--- Avalon/Avalon.List/Tree.cs
+++ Avalon/Avalon.List/Tree.cs
@@ -59,7 +59,7 @@ class Tree : Any
         return true;
     }
 
-    protected virtual bool Insert(TreeNode z)
+    public virtual bool Insert(TreeNode z)
     {
         TreeNode y;
         TreeNode x;
@@ -199,7 +199,7 @@ class Tree : Any
         return true;
     }
 
-    protected virtual bool Delete(TreeNode z)
+    public virtual bool Delete(TreeNode z)
     {
         TreeNode x;
         x = null;
